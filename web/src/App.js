088527be2fb82b6e6133c1587f9415b059787f52
Creon Creonopoulos <kreonjr@hotmail.com>
2021-10-26 11:16:21 -0500

    Pull and display all data from firebase realtime database

--- web/src/App.js
+++ web/src/App.js
@@ -1,22 +1,34 @@
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { firebase } from "./shared/firebaseConfig"
+import { getDatabase, ref, child, get } from "firebase/database";
 
 function App() {
+
+  const [data, setData] = useState({})
+
+  useEffect(() => {
+    const db = ref(getDatabase(firebase))
+    get(db).then((snapshot) => {
+      if (snapshot.exists()) {
+        setData(snapshot.val())
+      } else {
+        console.log("No data available");
+      }
+    }).catch((error) => {
+      console.error(error);
+    });
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h3>All Data</h3>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {JSON.stringify(data)}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
