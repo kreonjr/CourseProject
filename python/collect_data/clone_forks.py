4aacaec0f1d1bd80c949385bd930c3bb9e45a345
Kurt Tuohy <ktuohy@comcast.net>
2021-11-06 08:27:21 -0500

    Add scripts to pull text from past CS 410 project document files. Script output is included. Needs additional work. Some PDF text appears without spaces between words.
    Scripts:
    * clone_forks.py: clone all forks of a GitHub project, for forks that
    are at least n months old.
    * get_project_text.py: harvest text of PDFs, Word docs, Powerpoint
    files, and markdown files in those cloned projects.:
    
    Output files:
    * repo_forks.csv: names, links, and last-push dates for CS 410 projects.
    * project_file_text.tsv: the text of each document file. One line per
    project per document.
    * project_text.tsv: concatenated text of each project. One line per
    project.

---
+++ python/collect_data/clone_forks.py
@@ -0,0 +1,155 @@
+# -*- coding: utf-8 -*-
+"""
+clone_forks.py
+
+Find all forks of a GitHub repository, then
+locally clone them to a specified directory.
+Also output a CSV of forks to that directory.
+
+Arguments:
+    --forksurl: URL for GitHub API to retrieve desired forks
+    --destination: Local top-level folder to clone to
+    --doclone: whether to perform cloning step. "yes" to clone, "no" to simply collect info about the forks.
+    --minmonthsold: only process forks that were last pushed at least this number of months ago.
+
+Heavily based on findforks.py by Anand Kumria:
+    https://github.com/akumria/findforks
+
+Created on Sat Oct 30 14:09:23 2021
+
+@author: ktuoh
+"""
+
+import argparse
+import json
+#import subprocess
+import urllib.error
+import urllib.parse
+import urllib.request
+import os
+import pandas as pd
+from datetime import datetime
+from datetime import date
+import git
+
+def find_forks(forks_url):
+    """
+    Query the GitHub API for all forks of a repository.
+    """
+    resp_json = []
+
+    try:
+        resp = urllib.request.urlopen(forks_url)
+    except urllib.error.HTTPError as e:
+        if e.code == 404:
+            raise StopIteration
+
+    resp_json += json.loads(resp.read())
+
+    while github_resp_next_page(resp):
+        resp = urllib.request.urlopen(github_resp_next_page(resp))
+        resp_json += json.loads(resp.read())
+        
+    # Pull information about each fork into a dataframe
+    # Used ShikharDua's solution: https://stackoverflow.com/questions/10715965/create-a-pandas-dataframe-by-appending-one-row-at-a-time
+    forks_list = []
+    
+    for fork in resp_json:
+        fork_dict = {
+            "owner_login": fork["owner"]["login"],
+            "path": fork["full_name"],
+            "pushed_at": datetime.strptime(fork["pushed_at"], "%Y-%m-%dT%H:%M:%SZ"),
+            "ssh_url": fork["ssh_url"],
+            "project_url": fork["svn_url"]
+            }
+        
+        forks_list.append(fork_dict)
+        
+    # Return dataframe of forks.
+    return pd.DataFrame(forks_list)
+        
+
+def github_resp_next_page(resp):
+    """
+    Check to see if the GitHub response has a next link.
+    If the response, look for the 'link' header and see if
+    there is a value pointed to by next.
+    """
+    link_header = resp.getheader(u"link")
+
+    if not link_header:
+        return None
+
+    rel_next = u'rel="next"'
+    for link in link_header.split(u","):
+        if rel_next in link:
+            return link[link.find(u"<") + 1:link.rfind(u">")]
+
+    return None
+
+
+def min_months_old_met(date_1, date_2, min_months_old):
+    """
+    Determine whether two dates are at least min_months_old months apart.
+    Assume date_2 is greater than date_1.
+    Solution from https://www.tutorialsrack.com/articles/451/how-to-get-the-number-of-months-between-two-dates-in-python
+    """
+    return ((date_2.year - date_1.year) * 12) + (date_2.month - date_1.month) >= int(min_months_old)
+
+
+def shallow_clone_forks(forks_df, destination, min_months_old):
+    """
+    Given a dataframe of Git repo forks, clone all forks that are at least min_months_old.
+    Clone them to the "destination" folder. Use depth = 1 for a shallow clone.
+    """
+    
+    current_date = date.today() # Baseline for gauging age of last repo push
+    
+    # Loop through forks
+    for index, fork in forks_df.iterrows():
+        
+        # Clone forks at least min_months_old
+        if min_months_old_met(fork["pushed_at"], current_date, min_months_old):
+            
+            # Create subfolder for owner_login if it doesn't already exist
+            
+            fork_destination = os.path.join(destination, fork["owner_login"])
+            if (not os.path.isdir(fork_destination)):
+                try:
+                    os.makedirs(fork_destination)
+                    git.Repo.clone_from(fork["ssh_url"], fork_destination, branch=fork["default_branch"], depth=1)
+                except Exception:
+                    print("Error cloning ", fork["ssh_url"])
+                    continue
+            
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--forksurl", help="URL for GitHub API to retrieve desired forks", default="origin")
+    parser.add_argument("--destination", help="Local top-level folder to clone to", default=os.getcwd().replace("\\","/"))
+    parser.add_argument("--doclone", help="Whether to perform cloning step", default="no")
+    parser.add_argument("--minmonthsold", help="The minimum number of months since a repo was last pushed", default=3)
+    args = parser.parse_args()
+    
+    #print("args.forksurl: ", args.forksurl)
+
+    # Get dataframe of forks
+    forks_df = find_forks(args.forksurl)
+    
+    # First create the destination path if it doesn't exist.
+    if (not os.path.isdir(args.destination)):
+        os.makedirs(args.destination)
+
+    # Output dataframe of forks.
+    forks_df.to_csv(os.path.join(args.destination, "repo_forks.csv"), index=False)
+    
+    # If desired, clone forks of at least minimum age
+    if (args.doclone == "yes"):
+        shallow_clone_forks(forks_df, args.destination, args.minmonthsold)
+    
+    
+
+
+if __name__ == "__main__":
+    main()
---
+++ python/collect_data/get_project_text.py
@@ -0,0 +1,226 @@
+# -*- coding: utf-8 -*-
+"""
+get_project_text.py
+
+Pulls contents of textual files from locally-cloned GitHub projects.
+Handles .md, .pdf, .docx, .pptx.
+Stores text in two output files: one with a single row per project file,
+and another with a single row per project.
+
+Created on Sat Oct 30 20:55:24 2021
+
+@author: ktuoh
+"""
+
+import argparse
+import os
+import re
+from unidecode import unidecode
+import pandas as pd
+from pdfreader import SimplePDFViewer
+import docx2txt
+from pptx import Presentation
+from bs4 import BeautifulSoup
+from markdown import markdown
+
+
+# Arguments:
+    # projectlist: CSV list of projects.
+    # projectroot: root folder containing cloned projects.
+
+# Steps:
+    # Read in list of projects
+    # For each project:
+        # Look for textual files
+        # For each file:
+            # Get name
+            # Get contents
+            # Normalize whitespace
+            # Concatenate contents into single line
+            # Append project name, file name, and contents to data frame
+    # Output project/file contents
+    # Aggregate project/file contents into project contents
+    # Output project contents
+
+def get_pdf_text(filepath):
+    """
+    Return text contents of the given PDF.
+    Return as string.
+    """
+
+    filetext = ""
+    
+    try:
+        pdfObject = open(filepath, "rb")
+        pdfViewer = SimplePDFViewer(pdfObject)
+
+    # Handle incorrectly-formatted PDFs
+    except Exception:
+        print("Error opening PDF file", filepath)
+
+    # Pull text from readable PDFs
+    else:
+        # Loop through PDF canvas items and collect lists of doc strings.
+    
+        canvas_strings = []
+        
+        for canvas in pdfViewer:
+            canvas_strings.append(canvas.strings)
+            
+        # Flatten string list and return.
+    
+        for string_list in canvas_strings:
+            filetext = filetext + "".join(string_list)
+    
+    return filetext
+
+
+def get_docx_text(filepath):
+    """
+    Return text of the given Word document.
+    Return as string.
+    """
+    
+    filetext = ""
+
+    try:
+        filetext = docx2txt.process(filepath)
+    except Exception:
+        print("Error processing Word document", filepath)
+    
+    return filetext
+    
+
+def get_pptx_text(filepath):
+    """
+    Return text of the given Powerpoint document.
+    Return as string.
+    """
+    
+    filetext = ""
+    
+    ppt = Presentation(filepath)
+
+    # Loop through shapes in slides and collect any text.
+    for slide in ppt.slides:
+        for shape in slide.shapes:
+            if hasattr(shape, "text"):
+                filetext = filetext + " " + shape.text
+
+    return filetext
+
+def get_md_text(filepath):
+    """
+    Return flattened string consisting of the contents
+    of the given markdown file (or any text file).
+    """
+    
+    fd = open(filepath, "r", encoding="utf-8", errors="ignore")
+    md_text = fd.read()
+    fd.close()
+    
+    md_html = markdown(md_text)
+    filetext = "".join(BeautifulSoup(md_html, "html.parser").findAll(text=True))
+    
+    return filetext
+        
+    
+
+def normalize_text(input_text):
+    """
+    Given a string, remove or replace non-ASCII characters,
+    normalize spaces, and make text a single line.
+    """
+    
+    # Reduce text to single line.
+    norm_text = " ".join(input_text.splitlines()).strip()
+
+    # Normalize whitespace
+    norm_text = re.sub("\s+", " ", norm_text)
+
+    # Convert Unicode characters to their ASCII equivalents and return.
+    return unidecode(norm_text)
+
+
+def process_project_files(project_url, project_folder):
+    """
+    Given a project folder, identify textual files and pull contents.
+    Return the name of each file, plus the text as a single line.
+    Look in top level of directory only.
+    """
+    
+    filelist = []
+
+    for filename in os.listdir(project_folder):
+        filepath = os.path.join(project_folder, filename)
+        if os.path.isfile(filepath):
+            filetext = ""
+            if (filename.endswith(".pdf")):
+                filetext = get_pdf_text(filepath)
+            elif (filename.endswith(".docx")):
+                filetext = get_docx_text(filepath)
+            elif (filename.endswith(".pptx")):
+                filetext = get_pptx_text(filepath)
+            elif (filename.endswith(".md")):
+                filetext = get_md_text(filepath)
+                
+            if (filetext != ""):
+                # Remove or replace non-ASCII characters, normalize spaces, and make text a single line
+                normtext = normalize_text(filetext)
+                file_dict = {
+                    "project_url": project_url,
+                    "file_name": filename,
+                    "file_text": normtext
+                    }
+                filelist.append(file_dict)
+    
+    return filelist
+
+
+def process_project_list(project_df, project_root):
+    """
+    Iterate through dataframe of info about GitHub projects.
+    Look for textual files in each project, read the text, and log it.
+    project_list: Pandas dataframe
+    project_root: root directory of locally-cloned projects.
+    """
+    
+    doclist = []
+    
+    for index, row in project_df.iterrows():
+        project_folder = os.path.join(project_root, row["owner_login"])
+        if (os.path.isdir(project_folder)):
+            # Call routine to look for textual files in project_folder and collect their contents
+            doclist = doclist + process_project_files(row["project_url"], project_folder)
+            
+    return pd.DataFrame(doclist)
+
+
+def main():
+    
+    # Get arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--projectlist", help="CSV list of locally-cloned GitHub projects")
+    parser.add_argument("--projectroot", help="Root directory of locally-cloned GitHub projects", default=os.getcwd().replace("\\","/"))
+    args = parser.parse_args()
+
+    # Read project list
+    try:
+        project_df = pd.read_csv(args.projectlist)
+    except Exception:
+        print("Error reading project list")
+        exit()
+        
+    # Get dataframe of projects, textual files and their contents. One row per file.
+    project_file_df = process_project_list(project_df, args.projectroot)
+    # Output results
+    project_file_df.to_csv(os.path.join(args.projectroot, "project_file_text.tsv"), sep="\t", index=False)
+
+    # Concatenate text of each project's files and produce one row per project
+    project_text_series = project_file_df.groupby("project_url")["file_text"].agg(" ".join)
+    project_text_series.to_csv(os.path.join(args.projectroot, "project_text.tsv"), sep="\t", index=True)
+    
+
+if __name__ == "__main__":
+    main()
+    
