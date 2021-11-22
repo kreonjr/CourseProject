917070d48822b8cac3d1d4fe2b809e5071c63bad
sab6486 <suhas.bhat6486@gmail.com>
2021-11-22 15:45:07 +0530

    code change for new JSON structure

--- python/text_mining/text_to_JSON.py
+++ python/text_mining/text_to_JSON.py
@@ -1,49 +1,18 @@
 import json
 import pandas as pd
 
-data=pd.read_csv('AlgOutput.csv',header=None)
-Topic=data[1].unique() #picking unique topic values from file
-Tdict={} #Dictionary to hold all the topics, key-topic;values-sub dictionaries
-#Topic=['Intelligent Browsing','Intelligent Learning Platform','System Extension','Leaderboard Competition','Free Topics']
-FTopic=[] #List to hold the free topics
+df=pd.read_csv('AlgOutput.tsv',sep='\t')
+#data['Topics']
+#pd.Series(data.Topics.values,index=data.project_url).to_dict()
+topic_dict={} #Dictionary to hold all the topics
+for i in range(len(df)):
+    url_dict={} #dictionary to hold one url at a time
+    url_dict['url']=df['project_url'][i]
+    url_dict['tags']=df['Topics'][i]
+    topic_dict[i]=url_dict
 
-for t in Topic:
-    llist=[] #list to hold multiple dictionaries for different links within one topic
-    filein=open('AlgOutput.csv','r')
-    for line in filein:
-        ldict={} #Dictionary to hold the links, key - link;value - topic coverage
-        lineNew=line.strip() #removing endline character
-        words=lineNew.split(',') #splitting lines into words
-        if t == words[1]: #index 1 is where Topic name is stored
-            print(words[1])
-            if words[1]=='Free Topics':
-                if words[2] not in FTopic:
-                    FTopic.append(words[2])
-            else:
-                ldict['url']=words[0]
-                ldict['relevance']=words[3]
-                llist.append(ldict)
-    Tdict[t]=llist
-print(FTopic)
-Fdict={} #dictionary of subtopics under free topics, key - subtopic, value- sub dictionaries
-for ft in FTopic:
-    fllist=[] #list to hold multiple dictionaries for different links within one Free topic
-    filein=open('AlgOutput.csv','r')
-    for line in filein:
-        fldict={} #Dictionary to hold the links for subtopics under Free topics, key - link, value - topic coverage
-        lineNew=line.strip()
-        words=lineNew.split(',')
-        if ft==words[2]: #index 2 is where SubTopic name is stored
-            fldict['url']=words[0]
-            fldict['relevance']=words[3]
-            fllist.append(fldict)
-    Fdict[ft]=fllist
-Tdict['Free Topics']=Fdict
-
-print(Tdict)
-#     print(lineNew)
-#     print(words)
+#topic_dict
 
 fileout=open('firebase-output.json','w')
-json.dump(Tdict,fileout)
+json.dump(topic_dict,fileout)
 fileout.close()
