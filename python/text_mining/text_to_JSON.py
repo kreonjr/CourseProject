21a5fb9a60d6650e380a767c7acf1f92ec5a86c0
Creon Creonopoulos <kreonjr@hotmail.com>
2021-11-22 09:35:59 -0600

    Updated output script to replace single with double quotes

--- python/text_mining/text_to_JSON.py
+++ python/text_mining/text_to_JSON.py
@@ -8,7 +8,7 @@ topic_dict={} #Dictionary to hold all the topics
 for i in range(len(df)):
     url_dict={} #dictionary to hold one url at a time
     url_dict['url']=df['project_url'][i]
-    url_dict['tags']=df['Topics'][i]
+    url_dict['tags']=json.loads(df['Topics'][i].replace("'", '"'))
     topic_dict[i]=url_dict
 
 #topic_dict
