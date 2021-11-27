393f7c904c8688672d324a46f156731023dd976c
Creon Creonopoulos <kreonjr@hotmail.com>
2021-11-26 22:01:34 -0600

    Added some more high useage words. LDA.py comment cleanup

--- python/text_mining/LDA.py
+++ python/text_mining/LDA.py
@@ -9,23 +9,20 @@
 from nltk.tokenize import wordpunct_tokenize
 import pandas as pd
 import json
-#! pip install --upgrade gensim
 import gensim
 import gensim.corpora as corpora
-from ast import literal_eval #to convert file from a string as we are reading from .tsv
 
+# Convert file from a string as we are reading from .tsv
+from ast import literal_eval 
 df=pd.read_csv('project_clean_text.tsv',sep='\t',converters={'clean_text': literal_eval})
-#,converters={'clean_text': pd.eval})
-#df
 
-#Creating a mega list of each mega document's cleaned up tokens
+# Creating a mega list of each mega document's cleaned up tokens
 megalist=[]
 for i in range (len(df)):
     megalist.append(df['clean_text'][i])
-# megalist
-# len(megalist)
 
-dictionary=corpora.Dictionary(megalist) # creating a dictionary from the list of lists
+# Creating a dictionary from the list of lists
+dictionary=corpora.Dictionary(megalist) 
 
 # Term Document Frequency
 doc_term_matrix = [dictionary.doc2bow(doc, allow_update=True) for doc in megalist]
@@ -39,40 +36,40 @@ lda_model = gensim.models.ldamodel.LdaModel(corpus=doc_term_matrix,
                                            alpha='auto'
                                            )
 
-topic_word_dict={} #dictionary to capture the word distribution per topic
+# Dictionary to capture the word distribution per topic
+topic_word_dict={} 
 topics = lda_model.print_topics(num_words=10)
 for topic in topics:
-    #print(topic)
     topic_word_dict[topic[0]]=topic[1]
-#topic_word_dict
 
-doc_topic_dist_dict={} #dictionary to capture the topic distribution per mega document
+# Dictionary to capture the topic distribution per mega document
+doc_topic_dist_dict={} 
 count=0
 for i in lda_model[doc_term_matrix]:
-    #print('doc:',count,i)
     doc_topic_dist_dict[count]=i
     count+=1
-#doc_topic_dist_dict
 
-doc_topic_dist_final_dict={} #dictionary to capture the highest probability topic per mega document
+# Dictionary to capture the highest probability topic per mega document
+doc_topic_dist_final_dict={} 
 for url in range(len(df)):
     distribution=doc_topic_dist_dict[url]
     prob_dict={}
     for i in distribution:
         prob_dict[i[0]]=(i[1])
     doc_topic_dist_final_dict[url]=max(prob_dict, key=prob_dict.get)
-#doc_topic_dist_final_dict
 
-doc_topic_final_dict={} #dictionary to capture the words from the highest probability topic per mega document
+# Dictionary to capture the words from the highest probability topic per mega document
+doc_topic_final_dict={} 
 for url in range(len(df)):
     doc_topic_final_dict[url]=topic_word_dict[doc_topic_dist_final_dict[url]]
-#doc_topic_final_dict
 
-df['Topics']=df['clean_text'] #creating a new column as a clone of an existing one just as a placeholder
+# Creating a new column as a clone of an existing one just as a placeholder
+df['Topics']=df['clean_text'] 
 for url in range(len(df)):
-    df['Topics'][url]=doc_topic_final_dict[url] #assigning each row of the new column the captured words from the topic with hihgest prob
+    # Assigning each row of the new column the captured words from the topic with hihgest prob
+    df['Topics'][url]=doc_topic_final_dict[url] 
 
-#cleaning up the new column and tokenize
+# Cleaning up the new column and tokenize
 df['Topics1']=df['Topics'].str.replace('[^\w\s]',' ')
 df['Topics2']=df['Topics1'].str.replace('[\d]',' ')
 df['Topics3']=df['Topics2'].apply(wordpunct_tokenize)
@@ -80,22 +77,19 @@ df['Topics3']=df['Topics2'].apply(wordpunct_tokenize)
 Newdf=df[['project_url','file_text','clean_text','Topics3']]
 Newdf.rename(columns={'Topics3': 'Topics'},inplace=True)
 Newdf.set_index('project_url',inplace=True)
-#print(Newdf)
 Newdf.to_csv('AlgOutput.tsv', sep='\t')
 
 # Finally, generate JSON for the web app to consume.
-
 algoutput_df=pd.read_csv('AlgOutput.tsv',sep='\t')
-#data['Topics']
-#pd.Series(data.Topics.values,index=data.project_url).to_dict()
-topic_dict={} #Dictionary to hold all the topics
+
+#Dictionary to hold all the topics
+topic_dict={} 
 for i in range(len(df)):
-    url_dict={} #dictionary to hold one url at a time
+    # Dictionary to hold one url at a time
+    url_dict={} 
     url_dict['url']=algoutput_df['project_url'][i]
     url_dict['tags']=json.loads(algoutput_df['Topics'][i].replace("'", '"'))
     topic_dict[i]=url_dict
-
-#topic_dict
 
 fileout=open('firebase-output.json','w')
 json.dump(topic_dict,fileout)
